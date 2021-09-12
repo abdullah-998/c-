92bbbcf014e8fbc26f9c28495914695edd438682
unknown <abdullah.jayeedoo@gmail.com>
2021-09-12 14:58:12 +0600

    basic of c++ updated

--- base/bin_exp.cpp
+++ base/bin_exp.cpp
@@ -15,7 +15,19 @@ long long binexp(long long a,long long n){
     }
 }
 
+long long binExp(long long a,int n){
+    int ans=1;
+    while(n>0){
+        if(n%2==1){
+            ans = ans*a;
+        }
+        n=n>>1;
+        a=a*a;
+    }
+    return ans;
+}
+
 int main()
 {
-    cout<<binexp(25,45)<<endl;
+    cout<<binExp(2,5)<<endl;
 }
